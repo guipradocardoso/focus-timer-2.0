934a02d660a0e5d2b6e6886cc01b9cefeed8c5c5
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-09-20 23:49:10 -0300

    connecting first actions

---
+++ src/FocusTimer/actions.js
@@ -0,0 +1,13 @@
+export function toggleRunning() {
+  console.log("toggleRunning Function")
+}
+
+export function reset() {
+  console.log("reset Function")
+}
+export function plus() {
+  console.log("plus Function")
+}
+export function minus() {
+  console.log("minus Function")
+}
---
+++ src/FocusTimer/events.js
@@ -0,0 +1,22 @@
+import { controls, music } from "./elements.js"
+import * as actions from "./actions.js"
+
+export function registerControls() {
+  controls.addEventListener("click", (event) => {
+    const action = event.target.dataset.action
+    if (typeof actions[action] != "function") {
+      return
+    }
+    actions[action]()
+  })
+}
+
+export function registerMusic() {
+  music.addEventListener("click", (event) => {
+    const action = event.target.dataset.action
+    if (typeof actions[action] != "function") {
+      return
+    }
+    actions[action]()
+  })
+}
