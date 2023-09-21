7ad0611e41ab386720b28a3428a2fac73cb90b01
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-09-21 18:30:42 -0300

    updating the display

--- src/FocusTimer/elements.js
+++ src/FocusTimer/elements.js
@@ -1,2 +1,5 @@
 export const controls = document.getElementById("controls")
 export const music = document.getElementById("music")
+
+export const minutes = document.getElementById("minutes")
+export const seconds = document.getElementById("seconds")
