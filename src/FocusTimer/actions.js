15eb517f78b31c9232c36599cf50c56ed5bc5d00
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-09-21 18:11:46 -0300

    changing the application state

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -1,8 +1,13 @@
+import state from "./state.js"
+
 export function toggleRunning() {
+  state.isRunning = document.documentElement.classList.toggle("running")
   console.log("toggleRunning Function")
 }
 
 export function reset() {
+  state.isRunning = false
+  document.documentElement.classList.remove("running")
   console.log("reset Function")
 }
 export function plus() {
@@ -10,4 +15,21 @@ export function plus() {
 }
 export function minus() {
   console.log("minus Function")
+}
+
+export function toggleMusicForest() {
+  state.isMute = document.documentElement.classList.toggle("music-on")
+  console.log(toggleMusicForest)
+}
+export function toggleMusicRain() {
+  state.isMute = document.documentElement.classList.toggle("music-on")
+  console.log(toggleMusicRain)
+}
+export function toggleMusicCoffee() {
+  state.isMute = document.documentElement.classList.toggle("music-on")
+  console.log(toggleMusicCoffee)
+}
+export function toggleMusicFireplace() {
+  state.isMute = document.documentElement.classList.toggle("music-on")
+  console.log(toggleMusicFireplace)
 }
